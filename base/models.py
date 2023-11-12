7258111b4e232936904b0b030977ed60956b07a0
raihan-dip <79162559+raihan-dip@users.noreply.github.com>
2023-11-13 02:08:23 +0600

    adding

--- base/models.py
+++ base/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Topic(models.Model):
+    name= models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+class Room(models.Model):
+    
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null = True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null = True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null = True, blank= True)
+    #participants
+    updated = models.DateTimeField(auto_now=True)  # to see whenever it is updated
+    created = models.DateTimeField(auto_now_add=True) # it is used only once when it is created
+    
+    class Meta:
+        ordering= ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE) #using cascade to delete all messages of user.
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)  # to see whenever it is updated
+    created = models.DateTimeField(auto_now_add=True) # it is used only once when it is created
+    
+    def __str__(self):
+        return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -1,17 +1,47 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
-rooms= [
+'''rooms= [
     {'id': 1, 'name': 'Lets run python '},
     {'id': 2, 'name': 'Design with me'},
     {'id': 3, 'name': 'Frontend developer'},
-]
+]'''
 
 def home(request):
+    rooms = Room.objects.all()
     context= {'rooms' : rooms}
     return render(request, 'base/home.html', context)
 
 def room(request,pk):
-    return render(request, 'base/room.html')
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+        
+    return render(request, 'base/room.html',context)
+
+
+#crud 
+def createroom(request):
+    form = RoomForm()
+    if request.method == 'POST' :
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    context ={'form':form}
+    return render(request,'base/room_form.html', context)
+def updateroom(request,pk):
+    room =Room.objects.get(id =pk)
+    
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+         form = RoomForm(request.POST, instance=room)
+         if form.is_valid():
+             form.save()
+             return redirect('home')
+    
+    context = {'form': form}
+    return render(request,'base/room_form.html', context)
